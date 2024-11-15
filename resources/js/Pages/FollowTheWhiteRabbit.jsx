2e7995d50c7bda52e279b1837c2e098bd6ac6835
Simon <sijn82@gmail.com>
2024-11-15 18:16:46 +0000

    slight rewording of white rabbit article

--- resources/js/Pages/FollowTheWhiteRabbit.jsx
+++ resources/js/Pages/FollowTheWhiteRabbit.jsx
@@ -17,7 +17,7 @@ export default function FollowTheWhiteRabbit()
                         This is their backend challenge, and as Laravel is my go to backend framework, I set up a new controller and got to work.  
                         Knowing that there are three levels of difficulty I made a couple of assumptions.  
                         Firstly, as the anagram is made of three words, the easiest phrase at least, would likely be the same.  
-                        Secondly, that as the anagram didn't include an apostrophy the easiest phrase probably wouldn't either, and would be made up of words using characters without diacritic marks like accents and umlauts.  
+                        Secondly, as the anagram didn't include an apostrophy the easiest phrase probably wouldn't either, and would be made up of words using characters without diacritic marks like accents and umlauts.  
                         Which led me to a third assumption, that the hardest phase may be more than 3 words and will likely contain at least one word which contains a diacritic mark or apostrophy.
                     </div>
                     <div className='font-bold text-lg text-zinc-500 my-6'> Solution </div>
@@ -56,7 +56,7 @@ export default function FollowTheWhiteRabbit()
                         </div>
                         <div className='text-zinc-600 mb-3'>
                             The code below isn't the whole class as I wanted to keep the code examples down to enhance the narrative.  
-                            It also shows the tweaks I made to the code to optionally include a fourth word as I tried to complete the hardest challenge.  Prior to settling on this approach I experimented with a recursive function but the time taken to allow a fourth word increased the duration dramatically and when I allowed as many as required to use up all the anagram characters or clear the word list was ununsable.
+                            It also includes the tweaks I made to the code to optionally include a fourth word as I tried to complete the hardest challenge.  Prior to settling on this approach I experimented with a recursive function but the time taken to allow a fourth word increased the duration dramatically, so when I allowed as many as required to use up all the anagram characters or clear the word list was unusable.
                         </div>
                         <div className='my-6 border-lime-300 border-2 text-xs m-auto'>
                             <SyntaxHighlighter language="php" style={docco}>
@@ -66,12 +66,19 @@ export default function FollowTheWhiteRabbit()
                         <div className='font-bold text-lg text-zinc-500 my-6'> The Hardest Difficulty </div>
                         <div className='text-zinc-600 mb-3'>
                             After failing to find the hardest hash with the initial approach I reintroduced the words which were apostrophised and/or included diacritic marks. This added another 1000+ words into the loop and a few more considerations when comparing the word to the anagram.
-                            This increased the time taken to find the easier and medium challenges to 87 and 220 seconds respectively and led me down a few failed experiments to improve performance.  
-                            I believe saving the wordlist to a database would have had a positive impact but for some reason decided this was outside the scope of the challenge.
+                            Naturally, this also increased the time taken to find the easier and medium challenges, to 87 and 220 seconds respectively and led me down a few failed experiments to improve performance.  
+                        </div>
+                        <div className='text-zinc-600 mb-3'>
+                            On the topic of performance, I believe saving the wordlist to a database would have had a positive impact as it reduces how much is saved to memory during the process but for some reason decided this was outside the scope of the challenge.  
+                            That said, I might actually save it to the database* just to see what difference it makes because I rarely encounter such a great opportunity to directly test performance.  
+                            
+                        </div>
+                        <div className='text-zinc-600 mb-3'>
+                            <span className='italic text-xs'> *Though probably only locally as I'm not sure I want to pay the AWS RDS fees for production testing!</span>
                         </div>
                         <div className='text-zinc-600 mb-3'>
                             Finally I allowed a fourth word into the phrase and ran the code again.  
-                            Initially I increased the max duration time to 2 hrs but this wasn't sufficient, so I wrote a new function to start from where it had left off and ran it again. 
+                            Initially increasing the maximum duration time (PHP execution time) to 2 hrs but this wasn't sufficient, so I wrote a new function to start from where it had left off and ran it again. 
                             At this point I had decided to persevere until the code completed and I either had the solution or confirmation a rethink was required.  
                             Thankfully, after more than 8 hrs it did return the answer and after a few tweaks to the code I had the time taken down to 7536 seconds (or just over 2 hours and 5 mins).
                             While this is noticably quicker all it really comes down to is optimising the list I was looping over at each stage.
