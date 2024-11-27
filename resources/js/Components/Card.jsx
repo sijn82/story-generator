d65b9013d824d1cef47d5efe8fee049950944677
Simon <sijn82@gmail.com>
2024-11-27 15:04:19 +0000

    switched card suit svg's over to colour and added to s3

--- resources/js/Components/Card.jsx
+++ resources/js/Components/Card.jsx
@@ -4,16 +4,16 @@ export default function Card({card, showCard, setHand, zoom}) {
     return (
         <>
             {/* <div className={`rounded p-0.5 tall:p-2  border border-2 border-gray-400 text-gray-600 ${card.in_play ? 'bg-cyan-700' : 'bg-gray-600'} tall:h-32 medium:h-20 h-16 2xl:h-24 aspect-[3/4] grid grid-rows-4 ${zoom ? 'lg:h-24' : ''}`}  */}
-            <div className={`rounded p-0.5 tall:p-2  border border-2 border-gray-200 text-gray-600 ${card.in_play ? 'bg-gray-100' : 'bg-gray-600'} tall:h-32 medium:h-20 h-16 2xl:h-24 aspect-[3/4] grid grid-rows-4 ${zoom ? 'lg:h-24' : ''}`} 
+            <div className={`rounded p-0.5 tall:p-2 border-2 border-gray-200 text-gray-600 ${card.in_play ? 'bg-gray-100' : 'bg-gray-600'} tall:h-32 medium:h-20 h-16 2xl:h-24 aspect-[3/4] grid grid-rows-4 ${zoom ? 'lg:h-24' : ''}`} 
                 key={card.id} id={card.id} 
                 onClick={() => 
                     showCard(card, setHand)
             }>
                 <div className={`relative flex row-span-2`}>
                     <div 
-                        // style={{ backgroundImage: `url('https://sijn-portfolio.s3.eu-west-2.amazonaws.com/${card.suit}.svg')` }}
-                        style={{ backgroundImage: `url('http://localhost/storage/images/${card.suit.toLowerCase()}.svg')` }} 
-                        className={` bg-no-repeat absolute bg-contain top-0 right-0 tall:h-8 tall:w-8 medium:h-8 medium:w-8 h-6 w-6 xl:h-8 xl:h-8 `}>
+                        style={{ backgroundImage: `url('https://d1fftu7568zsov.cloudfront.net/${card.suit.toLowerCase()}.svg')` }}
+                        // style={{ backgroundImage: `url('http://localhost/storage/images/${card.suit.toLowerCase()}.svg')` }} 
+                        className={` bg-no-repeat absolute bg-contain top-0 right-0 tall:h-8 tall:w-8 medium:h-8 medium:w-8 h-6 w-6 xl:h-8 xl:w-8 `}>
 
                     </div>
                 </div>
